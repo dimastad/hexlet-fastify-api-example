9df1b212a826b312edfdabc68b3c6036e175c3f5
Dmitrii Portnov <dimastad@mac.local>
2026-04-01 17:03:27 +0400

    orm

--- routes/root.js
+++ routes/root.js
@@ -6,4 +6,12 @@ export default async function (fastify, opts) {
   fastify.get("/about", async function (request, reply) {
     return "Hexlet project";
   });
+
+  const { db } = fastify;
+
+  fastify.get("/users", async function (request, reply) {
+    const users = await db.query.users.findMany();
+
+    return users;
+  });
 }

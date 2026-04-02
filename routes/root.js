0c741905cb08a3690894348b94a6c04ec56f7306
Dmitrii Portnov <dimastad@mac.local>
2026-04-02 18:36:13 +0400

    tests

--- routes/root.js
+++ routes/root.js
@@ -1,6 +1,6 @@
 export default async function (fastify, opts) {
   fastify.get("/", async function (request, reply) {
-    return { root: false };
+    return { root: true };
   });
 
   fastify.get("/about", async function (request, reply) {
